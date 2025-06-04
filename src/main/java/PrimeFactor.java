f56d98faa56dd722af926e61b8c9a0fec2126600
magicers <magicers123@gmail.com>
2025-06-04 10:16:09 +0900

    [feature] 24

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactor {
-    public List<Integer> of(int i) {
+    public List<Integer> of(int number) {
         List<Integer> factors = new ArrayList<>();
+        if(number == 2)
+            factors.add(2);
         return factors;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -7,8 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PrimeFactorTest {
 
     @Test
-    void testPrimeFactor0f1() {
+    void testPrimeFactorOf1() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
+
+    @Test
+    void testPrimeFactorOf2() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
     }
 }
