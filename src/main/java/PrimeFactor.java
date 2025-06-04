49ed3973ece4231b115fc8e34824a300f63ba491
magicers <magicers123@gmail.com>
2025-06-04 10:24:28 +0900

    [feature] 39

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,8 +4,14 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> factors = new ArrayList<>();
-        if(number > 1)
-            factors.add(number);
+        if(number > 1){
+            if(number >= 4){
+                factors.add(2);
+                factors.add(2);
+            }else{
+                factors.add(number);
+            }
+        }
         return factors;
     }
 }
